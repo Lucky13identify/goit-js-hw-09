71210c548bec47840d65af76733c5e15a0787475
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-02-03 19:48:09 +0200

    fixed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,20 @@
 // Получаем доступы и вешаем слушателей
 
-const body = document.querySelector("body");
-const buttonStart = document.querySelector("[data-start]");
-const buttonStop = document.querySelector("[data-stop]");
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
 let timerID = null;
 
-buttonStart.addEventListener("click", () => {
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = 'true';
+});
+
+buttonStart.addEventListener('click', () => {
   timerID = setInterval(onBodyBgColor, 1000);
 });
-buttonStop.addEventListener("click", stopChangeBgColor);
+
+buttonStop.addEventListener('click', stopChangeBgColor);
 
 console.log(buttonStop);
 
@@ -22,14 +27,13 @@ function getRandomHexColor() {
 // Функция добавляет случайный цвет на body с интервалом
 
 function onBodyBgColor() {
-  buttonStart.disabled = "true";
   body.style.backgroundColor = getRandomHexColor();
 }
 
 // Функция возвращает стандарный фон и останавливает интервал
 
 function stopChangeBgColor() {
-  buttonStart.disabled = "";
+  buttonStart.disabled = '';
   clearInterval(timerID);
-  body.style.backgroundColor = "";
+  body.style.backgroundColor = '';
 }
